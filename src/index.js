1624bb35ca6af7e6b5a95e1e0deed4a588b8f9c0
Vampsas <sandra.szablewska@onet.pl>
2022-05-09 20:37:29 +0200

    declaration of which player`s turn

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import {createRoot} from "react-dom/client";
 import './App.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square" onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -17,27 +15,29 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
-    handleClick(i){
+
+    handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
+
     renderSquare(i) {
-        return (
-            <Square
+        return (<Square
                 value={this.state.squares[i]}
                 onClick={() => this.handleClick(i)}
-            />
-        );
+            />);
     }
 
     render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        return (<div>
                 <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
@@ -54,15 +54,13 @@ class Board extends React.Component {
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </div>
-            </div>
-        );
+            </div>);
     }
 }
 
 class Game extends React.Component {
     render() {
-        return (
-            <div className="game">
+        return (<div className="game">
                 <div className="game-board">
                     <Board/>
                 </div>
@@ -70,8 +68,7 @@ class Game extends React.Component {
                     <div>{/* status */}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
-            </div>
-        );
+            </div>);
     }
 }
 
